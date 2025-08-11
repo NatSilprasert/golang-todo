2b8be8c95c06a2813bfbca1f61a8efd2bd13c14a
Nat Silprasert <natsilprasert@gmail.com>
2025-08-11 16:29:58 +0700

    Implement Auth & Todo API

---
+++ backend/handlers/jwt_middleware.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func JWTProtected() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		authHeader := c.Get("Authorization")
+		if authHeader == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Missing or malformed JWT"})
+		}
+
+		tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+
+		if err != nil || !token.Valid {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid or expired JWT"})
+		}
+
+		// Save user id to locals for later use
+		claims := token.Claims.(jwt.MapClaims)
+		c.Locals("userId", uint(claims["id"].(float64)))
+
+		return c.Next()
+	}
+}
