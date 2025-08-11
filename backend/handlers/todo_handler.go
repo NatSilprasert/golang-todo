a736fe90bd74d1690675a318fbcd15b1c47cb55c
Nat Silprasert <natsilprasert@gmail.com>
2025-08-11 16:29:58 +0700

    Implement Auth & Todo API

--- backend/handlers/todo_handler.go
+++ backend/handlers/todo_handler.go
@@ -1,1 +1,66 @@
 package handlers
+
+import (
+	"github.com/NatSilprasert/golang-todo/backend/config"
+	"github.com/NatSilprasert/golang-todo/backend/models/todo"
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetTodos(c *fiber.Ctx) error {
+	userId := c.Locals("userId").(uint)
+
+	var todos []todo.Todo
+	if err := config.DB.Where("user_id = ?", userId).Find(&todos).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to get todos"})
+	}
+
+	return c.JSON(todos)
+}
+
+func CreateTodo(c *fiber.Ctx) error {
+	userId := c.Locals("userId").(uint)
+
+	todo := new(todo.Todo)
+	if err := c.BodyParser(todo); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+	}
+
+	todo.UserID = userId
+
+	if err := config.DB.Create(&todo).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to create todo"})
+	}
+
+	return c.JSON(fiber.Map{"message": "Todo added"})
+}
+
+func UpdateTodo(c *fiber.Ctx) error {
+    userId := c.Locals("userId").(uint)
+    id := c.Params("id")
+
+    var todoItem todo.Todo
+    if err := config.DB.Where("id = ? AND user_id = ?", id, userId).First(&todoItem).Error; err != nil {
+        return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Todo not found"})
+    }
+
+    if err := c.BodyParser(&todoItem); err != nil {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+    }
+
+    if err := config.DB.Save(&todoItem).Error; err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update todo"})
+    }
+
+    return c.JSON(fiber.Map{"message": "Todo updated"})
+}
+
+func DeleteTodo(c *fiber.Ctx) error {
+    userId := c.Locals("userId").(uint)
+    id := c.Params("id")
+
+    if err := config.DB.Where("id = ? AND user_id = ?", id, userId).Delete(&todo.Todo{}).Error; err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to delete todo"})
+    }
+
+    return c.JSON(fiber.Map{"message": "Todo deleted"})
+}
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,1 +1,20 @@
 package routes
+
+import (
+	"github.com/NatSilprasert/golang-todo/backend/handlers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Setup(app *fiber.App) {
+
+	app.Post("/api/register", handlers.Register)
+	app.Post("/api/login", handlers.Login)
+
+	api := app.Group("/api", handlers.JWTProtected())
+
+	api.Get("/todos", handlers.GetTodos)
+	api.Post("/todos", handlers.CreateTodo)
+	app.Put("/todos/:id", handlers.UpdateTodo)
+	app.Delete("/todos/:id", handlers.DeleteTodo)
+}
