777208534869248dd115dec95b25f2f6eacb86a1
Nat Silprasert <natsilprasert@gmail.com>
2025-08-11 15:48:15 +0700

    Implement Backend

---
+++ backend/models/todo/todo.go
@@ -0,0 +1,13 @@
+package todo
+
+import "time"
+
+type Todo struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	UserID    uint      `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DueDate   time.Time `json:"due_date"`
+}
