6ca1980ad8fa189712e42b36582aba8555301258
Nat Silprasert <natsilprasert@gmail.com>
2025-08-18 00:08:36 +0700

    Add Logout Feature

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,18 @@
-import { ArrowRight, Check, } from "lucide-react"
+import { ArrowLeft, ArrowRight, Check, } from "lucide-react"
 import { Link } from "react-router-dom"
+import { useAppContext } from "../context/AppContext"
 
 const Navbar = () => {
+
+  const {token, setToken} = useAppContext();
+
+  const buttonHandler = () => {
+    if (token) {
+      localStorage.removeItem('token')
+      setToken('')
+    }
+  }
+
   return (
     <div className="px-24 py-4 border-b-4 border-bg2 flex justify-between">
         <div className="flex gap-1 items-center">
@@ -10,9 +21,12 @@ const Navbar = () => {
         </div>
 
         <Link to='/login'>
-            <button className="px-6 py-3 bg-bg2 rounded-2xl flex gap-2 items-center hover:bg-bg-hover/30">
-                <ArrowRight size={16} />
-                    <p className="">Login</p>
+            <button 
+              onClick={buttonHandler}
+              className="px-6 py-3 bg-bg2 rounded-2xl flex gap-2 items-center hover:bg-bg-hover/30"
+            >
+              {token ? <ArrowLeft size={16} /> : <ArrowRight size={16} /> }
+              <p>{token ? 'Logout' : 'Login'}</p>
             </button>
         </Link>
     </div>
